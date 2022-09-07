e25e9f173ce0526bd30172a4460b0da1bff97548
holasoynaoki <naoki13futsal@gmail.com>
2022-09-07 15:45:07 +0900

    setup for prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule.forRoot({ isGlobal: true })],
   controllers: [AppController],
   providers: [AppService],
 })
